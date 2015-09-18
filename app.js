d9b470204240599aa65ca76260ade5eb96cc077e
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-18 00:21:34 -0700

    changed some files

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ $(function(){
   //create our octocat template
   var octocatTemplate = _.template($('#octocat-template').html());
   // data is defined in data.js
-  var octocats = data.octocats;
+   octocats = data.octocats;
   // get our template to give us an html string for each octocat
-  octocats.forEach(function(octocat){
+  octocats.forEach(function (student){
     // give our template some data and get html from it
-    var octocatHtml = octocatTemplate(octocat);
+    var octocatHtml = octocatTemplate(student);
     // append resulting html to the placeholder area
     $("#octocats-placeholder").append(octocatHtml);
   });
+
+
+
 });
+
+
+
+
+
+
